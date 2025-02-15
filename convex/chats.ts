d11c3e7bf622ecffe6adb5d291550187608a3967
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-02-15 23:41:36 +0530

    Update chats.ts

--- convex/chats.ts
+++ convex/chats.ts
@@ -50,7 +50,7 @@ export const update = mutation({
 export const remove = mutation({
   args: { chatId: v.id("chats") },
   handler: async (ctx, args) => {
-    // Delete all messages
+    // Delete all
     const messages = await ctx.db
       .query("messages")
       .filter((q) => q.eq(q.field("chatId"), args.chatId))
